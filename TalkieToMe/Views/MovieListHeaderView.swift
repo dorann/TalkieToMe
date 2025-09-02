3c175d807907d50ce223c860449729fa72ed10dd
dorann <koras.d@icloud.com>
2025-09-01 19:12:46 -0700

    Add a movie list header view to display header text and count of movies

---
+++ TalkieToMe/Views/MovieListHeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListHeaderView.swift
+//  TalkieToMe
+//
+//  Created by dermetma on 9/1/25.
+//
+
+import SwiftUI
+
+struct MovieListHeaderView: View {
+    var movieCount: Int
+    
+    var body: some View {
+        HStack {
+            Text("To Watch")
+                .foregroundColor(.black)
+            Text(String(movieCount))
+                .padding(EdgeInsets(top: 2, leading: 7, bottom: 2, trailing: 7))
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 9))
+                .foregroundColor(.white)
+        }
+
+    }
+}
+
+#Preview {
+    let movieCount: Int = 70
+    MovieListHeaderView(movieCount: movieCount)
+}
