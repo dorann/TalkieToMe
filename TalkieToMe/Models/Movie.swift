9b875a6e7ccfebe40225db6f1aa422b38569a046
dorann <koras.d@icloud.com>
2025-09-01 17:13:51 -0700

    Add movie model and preview content

---
+++ TalkieToMe/Models/Movie.swift
@@ -0,0 +1,36 @@
+//
+//  Movie.swift
+//  TalkieToMe
+//
+//  Created by dermetma on 9/1/25.
+//
+
+import Foundation
+import SwiftData
+import SwiftUICore
+
+@Model
+class Movie: Identifiable {
+    var id: UUID
+    var title: String
+    var rating: String
+    var releaseYear: Int
+    var runTime: Int
+    var dateWatched: Date?
+    var dateAdded: Date
+    
+    var image: Image {
+        let convertedTitle = title.lowercased().replacingOccurrences(of: " ", with: "")
+        return Image(convertedTitle)
+    }
+    
+    init(id: UUID = UUID(), title: String, rating: String, releaseYear: Int, runTime: Int, dateWatched: Date? = nil) {
+        self.id = id
+        self.title = title
+        self.rating = rating
+        self.releaseYear = releaseYear
+        self.runTime = runTime
+        self.dateAdded = Date()
+        self.dateWatched = dateWatched
+    }
+}
---
+++ TalkieToMe/Preview Content/Movie+Sample.swift
@@ -0,0 +1,43 @@
+//
+//  Movie+Sample.swift
+//  TalkieToMe
+//
+//  Created by dermetma on 9/1/25.
+//
+
+import Foundation
+
+extension Movie {
+    static let sampleData: [Movie] =
+    [
+        Movie(title: "The Conference",
+              rating: "R",
+              releaseYear: 2023,
+              runTime: 101),
+        Movie(title: "Until Dawn",
+              rating: "R",
+              releaseYear: 2025,
+              runTime: 103
+             ),
+        Movie(title: "The Evil Dead",
+              rating: "NC-17",
+              releaseYear: 1981,
+              runTime: 85
+             ),
+        Movie(title: "The Thing",
+              rating: "R",
+              releaseYear: 1982,
+              runTime: 109
+             ),
+        Movie(title: "Clown in a Cornfield",
+              rating: "R",
+              releaseYear: 2025,
+              runTime: 96
+             ),
+        Movie(title: "It's a Wonderful Knife",
+              rating: "R",
+              releaseYear: 2023,
+              runTime: 87
+             )
+    ]
+}
